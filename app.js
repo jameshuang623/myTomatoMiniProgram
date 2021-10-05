ad19ffffaea5b0b7de12a8d056910dd6dd71cf10
jameshuang623 <363753466@qq.com>
2021-10-06 00:25:14 +0800

    一键登录

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 App({
   globalData: {
     host: 'https://gp-server.hunger-valley.com',  //照抄
-    app_id: "\twx7412dbf0d361af77",
+    app_id: "wx7412dbf0d361af77",
     app_secret: "fc94312fc215d9112838a8721171ea51",
     t_app_id: "duhMqJM63uaJ9KcvB8wEDksg",
     t_app_secret: "33B33W2cgoFiJFKujG82dgmu"
